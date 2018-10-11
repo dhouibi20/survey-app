00467b717a75e57f31053d0f172f8a298638e2c5
Mohamed Dhouibi <mdhouibi@Mohameds-MacBook-Pro.local>
2018-10-12 00:23:10 +0100

    remove redux devtool

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,17 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-const composeEnhancers = composeWithDevTools({
-    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-  });
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-  ));
+// const composeEnhancers = composeWithDevTools({
+//     // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+//   });
+// const store = createStore(rootReducer, composeEnhancers(
+//     applyMiddleware(thunk)
+//   ));
+
+const store = createStore(rootReducer, 
+  applyMiddleware(thunk)
+);
 
   export default store;
